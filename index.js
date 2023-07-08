55a5d1f54e8dfca2b1a4715e0e44e0edafd39d82
anasalmezyd <Anas.almziad@gmail.com>
2023-07-08 09:23:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+    
+    function clickAlert() {
+        alert('I was clicked!');
+    }
+    
+    input.addEventListener('click', clickAlert);
 }
+
+// This line is added to automatically call the addingEventListener function upon loading the page.
+// So, we do not have to manually call it from the browser console.
+addingEventListener();
